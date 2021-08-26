862b86df1c54e57c362ea8e44033e56c8a649373
Ryid <dabau975@gmail.com>
2021-08-26 23:19:35 +0800

    0.5ver

--- Bingo/src/components/bingo.vue
+++ Bingo/src/components/bingo.vue
@@ -16,7 +16,11 @@ export default {
       rows: 5,
       cols: 5,
       myNums: "",
-      getNum: [1, 2, 3, 4, 5],
+      getNum: [
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+        21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38,
+        39, 40, 41, 42, 43, 44, 45,
+      ],
       indices: [],
     };
   },
@@ -55,14 +59,72 @@ export default {
         .catch((err) => console.log(err));
       await this.getServerNum();
     },
-    clickSquare(e) {
-      console.log(e.innerHTML);
+    clickSquare(square) {
+      console.log(square.innerHTML);
       this.getNum.filter((num) => {
-        if (num == e.innerHTML) {
-          e.style.backgroundColor = "crimson";
-          this.indices.push(e.index);
+        if (num == square.innerHTML) {
+          square.style.backgroundColor = "crimson";
+          this.indices.push(square.index);
+          this.checkWin();
         }
       });
+    },
+    checkArray(number, array) {
+      // console.log("Checking array for" + number);
+      for (let count = 0; count < array.length; count++) {
+        if (array[count] == number) {
+          return true;
+        }
+      }
+      return false;
+    },
+    check5(i1, i2, i3, i4, i5) {
+      return (
+        this.checkArray(i1, this.indices) &&
+        this.checkArray(i2, this.indices) &&
+        this.checkArray(i3, this.indices) &&
+        this.checkArray(i4, this.indices) &&
+        this.checkArray(i5, this.indices)
+      );
+    },
+    checkWin() {
+      console.log(this.indices);
+      if (this.check5(0, 1, 2, 3, 4)) {
+        console.log("Bingo!");
+      }
+      if (this.check5(5, 6, 7, 8, 9)) {
+        console.log("Bingo!");
+      }
+      if (this.check5(10, 11, 12, 13, 14)) {
+        console.log("Bingo!");
+      }
+      if (this.check5(15, 16, 17, 18, 19)) {
+        console.log("Bingo!");
+      }
+      if (this.check5(20, 21, 22, 23, 24)) {
+        console.log("Bingo!");
+      }
+      if (this.check5(0, 5, 10, 15, 20)) {
+        console.log("Bingo!");
+      }
+      if (this.check5(1, 6, 11, 16, 21)) {
+        console.log("Bingo!");
+      }
+      if (this.check5(2, 7, 12, 17, 22)) {
+        console.log("Bingo!");
+      }
+      if (this.check5(3, 8, 13, 18, 23)) {
+        console.log("Bingo!");
+      }
+      if (this.check5(4, 9, 14, 19, 24)) {
+        console.log("Bingo!");
+      }
+      if (this.check5(0, 6, 12, 18, 24)) {
+        console.log("Bingo!");
+      }
+      if (this.check5(4, 8, 12, 16, 20)) {
+        console.log("Bingo!");
+      }
     },
   },
   computed: {},
